011fea53fb7b52ece5809413d939c81b4246c359
jithendra <jkumar@komhar.com>
2022-07-17 02:53:13 +0530

    20220717

---
+++ scripts/join.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+
+df_emp=pd.read_csv('log/emp/csv')
+print(df_emp)
+
+df_dept=pd.read_csv('log/dept.csv')
+
+print(df_dept)
--- scripts/read_csv.py
+++ scripts/read_csv.py
@@ -1,9 +1,20 @@
+from cmath import nan
+from numpy import NAN, NaN
 import pandas as pd
 import os
 
 
+df = pd.read_csv('log/test.csv',header=0)
 
-df = pd.read_csv('./log/file_list.txt')
+for idx in df.index:
+    if (df.loc[idx,'id']):
+        df.loc[idx,'name']
 
-for line in df.items():
-    print(line)
+# for idx in df.index:
+
+#     if(df.loc[idx,'id']<4):
+#         df.loc[idx,'id']=100
+#     else:
+#         df.loc[idx,'id']=200
+
+# df.to_csv('log/test.csv')
